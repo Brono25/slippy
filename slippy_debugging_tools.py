bce7f4edada6a92866c3837ca79961199b83b19d
z5146619 <brono_25@hotmail.com>
2022-07-25 01:07:55 +1000

    make parseInput

--- slippy_debugging_tools.py
+++ slippy_debugging_tools.py
@@ -2,41 +2,20 @@
 
 
 
-def printAddress(addr):
-
-    if addr.is_address_found == False:
-        print('No Address')
-
-    if addr.single_num != None:
-        print(r'single num:',addr.single_num)
-
-    if addr.single_regexp != None:
-        print(r'single regex:',addr.single_regexp)
-
-    if addr.start_num != None:
-        print(r'start num',addr.start_num)
-
-    if addr.start_regexp != None:
-        print(r'start regex',addr.start_regexp)
-
-    if addr.end_num != None:
-        print(r'end num',addr.end_num)
-
-    if addr.end_regexp != None:
-        print(r'end regex',addr.end_regexp)
-
-    if addr.address_string != None:
-        print(r'Full string:',addr.address_string)
 
 
 def printCommand(cmd):
 
     print('operation:', cmd.operation)
-
-    if cmd.operation == 's':
-        print('delimeter:', cmd.delimiter)
-        print('replace:', cmd.replace_regexp)
-        print('replace:', cmd.substitute)
-        print('flag:', cmd.flag)
+    print('replace:', cmd.s_replace)
+    print('sub:', cmd.s_substitute)
+    print('flag:', cmd.s_global_flag)
+    print(r'single num:',cmd.single_num)
+    print(r'single regex:',cmd.single_regexp)
+    print(r'start num',cmd.start_num)
+    print(r'start regex',cmd.start_regexp)
+    print(r'end num',cmd.end_num)
+    print(r'end regex',cmd.end_regexp)
+    print(r'Full string:',cmd.full_address)
 
 
--- slippy_parse_input.py
+++ slippy_parse_input.py
@@ -8,16 +8,8 @@ import slippy_utility as util
 
 class Command:
     def __init__(self):
-        self.operation  = None
-        self.delimiter  = '/'
-        self.replace    = None
-        self.substitute = None
-        self.flag       = None
-
-
-
-class Address:
-    def __init__(self):
+        self.operation        = None
+        #addresses
         self.is_address_found = False
         self.single_num       = None
         self.single_regexp    = None
@@ -25,18 +17,19 @@ class Address:
         self.start_regexp     = None
         self.end_num          = None
         self.end_regexp       = None
-        self.address_string   = None
-        self.regex_found      = False
+        self.full_address     = None
+        #subs
+        self.s_replace        = None
+        self.s_substitute     = None
+        self.s_global_flag    = False
 
 
 
-def getAddress(slippy_cmd):
+
+def getAddressInfo(cmd, cmd_info):
     """ 
     Retreive the address for a given slippy command
     """
-
-    cmd  = slippy_cmd.strip()
-    addr_info = Address()
 
     no_adrr     = r'^[qpds]'
     single_num  = r'(^(\d+)\s*)[qpds]'
@@ -48,89 +41,83 @@ def getAddress(slippy_cmd):
 
 
     if result := re.search(no_adrr, cmd):
-        addr_info.is_address_found  = False
+        cmd_info.is_address_found  = False
 
     elif result := re.search(regx_regx, cmd):
-        addr_info.address_string = result.group(1)
-        addr_info.start_regexp   = result.group(2)
-        addr_info.end_regexp     = result.group(3)
-        addr_info.is_address_found  = True
+        cmd_info.full_address     = result.group(1)
+        cmd_info.start_regexp     = result.group(2)
+        cmd_info.end_regexp       = result.group(3)
+        cmd_info.is_address_found = True
 
     elif result := re.search(regx_num, cmd):
-        addr_info.address_string = result.group(1)
-        addr_info.start_regexp   = result.group(2)
-        addr_info.end_num        = int(result.group(3))
-        addr_info.is_address_found  = True
+        cmd_info.full_address     = result.group(1)
+        cmd_info.start_regexp     = result.group(2)
+        cmd_info.end_num          = int(result.group(3))
+        cmd_info.is_address_found = True
 
     elif result := re.search(num_regx, cmd):
-        addr_info.address_string = result.group(1)
-        addr_info.start_num      = int(result.group(2))
-        addr_info.end_regexp     = result.group(3)
-        addr_info.is_address_found  = True
+        cmd_info.full_address = result.group(1)
+        cmd_info.start_num    = int(result.group(2))
+        cmd_info.end_regexp   = result.group(3)
+        cmd_info.is_address_found  = True
 
     elif result := re.search(num_num, cmd):
-        addr_info.address_string = result.group(1)
-        addr_info.start_num      = int(result.group(2))
-        addr_info.end_num        = int(result.group(3))
-        addr_info.is_address_found  = True
+        cmd_info.full_address = result.group(1)
+        cmd_info.start_num    = int(result.group(2))
+        cmd_info.end_num      = int(result.group(3))
+        cmd_info.is_address_found  = True
         
     elif result := re.search(single_num, cmd):
-        addr_info.address_string = result.group(1)
-        addr_info.single_num     = int(result.group(2))
-        addr_info.is_address_found  = True
+        cmd_info.full_address = result.group(1)
+        cmd_info.single_num   = int(result.group(2))
+        cmd_info.is_address_found  = True
 
     elif result := re.search(single_regx, cmd):
-        addr_info.address_string = result.group(1)
-        addr_info.single_regexp  = result.group(2)
-        addr_info.is_address_found  = True
+        cmd_info.full_address  = result.group(1)
+        cmd_info.single_regexp = result.group(2)
+        cmd_info.is_address_found  = True
 
     else:
         util.printInvalidCommand()
 
-    return addr_info
+
+    
+
+    return cmd_info
 
 
 
 
-def getCommand(input_cmd, addr_string):
+def getCommandInfo(cmd, cmd_info):
 
 
-    cmd_isolated = input_cmd.strip()
-
-    #if address is present strip it from the input command
-    if addr_string != None:
-        cmd_isolated = re.sub(re.escape(addr_string), '', cmd_isolated)
-
-
-    cmd_info = Command()
-
-    if result := re.search(r'^q', cmd_isolated):
+    if result := re.search(r'^q', cmd):
         cmd_info.operation = result.group()
         
-    elif result := re.search(r'^p', cmd_isolated):
+    elif result := re.search(r'^p', cmd):
         cmd_info.operation = result.group()
 
-    elif result := re.search(r'^d', cmd_isolated):
+    elif result := re.search(r'^d', cmd):
         cmd_info.operation = result.group()
 
-    elif result := re.search(r'^s', cmd_isolated):
+    elif result := re.search(r'^s', cmd):
         cmd_info.operation = result.group()
 
     #parse substitute operation
     if cmd_info.operation == 's':
 
-        d = cmd_isolated[1] #delimeter is first character after 's'
+        d = cmd[1] #delimeter is first character after 's'
         cmd_info.delimiter = d
 
         s_regex = '^s' + d + r'(.*)' + d + r'(.*)' + d + r'(.*)$'
 
-        if result := re.search(s_regex, cmd_isolated):
+        if result := re.search(s_regex, cmd):
 
-            cmd_info.replace = result.group(1)
-            cmd_info.substitute = result.group(2)
+            cmd_info.s_replace = result.group(1)
+            cmd_info.s_substitute = result.group(2)
 
             if result.group(3) == 'g':
-                cmd_info.flag = result.group(3)
+                cmd_info.s_global_flag = True
 
             elif result.group(3) == '':
                 pass
@@ -140,14 +127,29 @@ def getCommand(input_cmd, addr_string):
         else:
             util.printInvalidCommand()
 
+    return cmd_info
+ 
 
 
 
+
+def parseInput(input_cmd):
+
+
+    input_cmd  = input_cmd.strip()
+
+    cmd_info = Command()
+
+    cmd_info = getAddressInfo(input_cmd, cmd_info)
+
+    #Strip address from command 
+    cmd_stripped = input_cmd
+    if cmd_info.full_address != None:
+        cmd_stripped = re.sub(re.escape(cmd_info.full_address), '', cmd_stripped)
+
+    cmd_info = getCommandInfo(cmd_stripped, cmd_info)
 
     return cmd_info
-    
-
-
 
 
 
